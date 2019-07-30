8ff69d4c194ee420d065eacf07241566da10fb7f
Astrid88 <giu.munari@gmail.com>
2019-07-30 16:22:36 +0200

    add the store

--- App.js
+++ App.js
@@ -2,15 +2,23 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import SignUp from './screens/SignUp';
 
+import store from './store'
+import { Provider } from 'react-redux'
+
+import { BrowserRouter } from 'react-router-dom'
 
 export default class App extends React.Component {
   render() {
     return (
-    <View style={styles.container}>
-      <SignUp />
-    </View>
-  );
-}
+      <BrowserRouter>
+        <Provider store={store}>
+          <View style={styles.container}>
+            <SignUp />
+          </View>
+        </Provider>
+      </BrowserRouter>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
