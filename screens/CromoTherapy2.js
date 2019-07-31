3cdfd4ab4b4209d239816cb2081e90050ec4c645
Astrid88 <giu.munari@gmail.com>
2019-07-31 11:38:17 +0200

    add animated background

---
+++ screens/CromoTherapy2.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { View, Text, TouchableHighlight, Animated, StyleSheet } from 'react-native';
+import styles from './ChromoTherapy.styles';
+import { connect } from 'react-redux';
+import { Actions } from 'react-native-router-flux';
+class ChromoTherapy extends Component {
+    constructor(props) {
+        super(props);
+        // Intialize to default value
+        this.state = {
+            backgroundColor: new Animated.Value(0)
+        };
+    }
+    componentDidMount() {
+        this.setState({ backgroundColor: new Animated.Value(0) }, () => {
+            Animated.timing(this.state.backgroundColor, {
+                toValue: 100,
+                duration: 5000
+            }).start();
+        });
+    }
+
+    render() {
+        return (
+            <Animated.View
+                style={[
+                    styles.container,
+                    // Interpolation mapping from numbers to strings (colors)
+                    {
+                        backgroundColor: this.state.backgroundColor.interpolate({
+                            inputRange: [0, 100, 200, 300],
+                            outputRange: ['#ff9f43', '#1dd1a1', '#54a0ff', '#5f27cd']
+                        })
+                    }
+                ]}
+            >
+                <Text
+                    onPress={Actions.lounchSession}
+                    style={styles.paragraph}
+                >
+                    Stop
+            </Text>
+            </Animated.View>
+        );
+    }
+}
+
+
+function mapStateToProps(state) {
+    return {
+        //message: state.message.message
+    }
+}
+export default connect(mapStateToProps)(ChromoTherapy)
